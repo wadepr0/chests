924fdef9e277947c869653fab9eecefec445b2f1
wadepr0 <67891615+wadepr0@users.noreply.github.com>
2022-06-04 22:13:31 +0300

    90% done

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import BoardComponent from './components/BoardComponent';
+import './App.css'
+import { Board } from './models/Board';
+import { Player } from './models/Player';
+import { Colors } from './models/Colors';
 
 const App = () => {
+  const [board, setBoard] = useState(new Board())
+  const [whitePlayer, setWhitePlayer] = useState(new Player(Colors.WHITE))
+  const [blackPlayer, setBlackPlayer] = useState(new Player(Colors.BLACK))
+  useEffect(() => {
+    restart()
+  }, [])
+  function restart() {
+    const newBoard = new Board();
+    newBoard.initCells()
+    newBoard.addFigures()
+    setBoard(newBoard)
+  }
   return (
-    <div>
-
+    <div className='app'>
+      <BoardComponent
+        board={board}
+        setBoard={setBoard}
+      />
     </div>
   );
 };
---
+++ src/components/BoardComponent.tsx
@@ -0,0 +1,54 @@
+import React, { FC, useEffect, useState } from 'react'
+import { Board } from '../models/Board'
+import { Cell } from '../models/Cell'
+import CellComponent from './CellComponent'
+
+interface BoardProps {
+    board: Board;
+    setBoard: (board: Board) => void;
+}
+
+const BoardComponent: FC<BoardProps> = ({ board, setBoard }) => {
+    const [selectedCell, setSelectedCell] = useState<Cell | null>(null)
+
+    useEffect(() => {
+        highLightCells()
+    }, [selectedCell])
+
+    function click(cell: Cell) {
+        if (selectedCell && selectedCell !== cell && selectedCell.figure?.canMove(cell)) {
+            selectedCell.moveFigure(cell)
+            setSelectedCell(null)
+        } else {
+            setSelectedCell(cell)
+        }
+    }
+
+    function highLightCells() {
+        board.highLightCells(selectedCell)
+        updateBoard()
+    }
+    function updateBoard() {
+        const newBoard = board.getCopyBoard()
+        setBoard(newBoard)
+    }
+    return (
+        <div className='board'>
+            {board.cells.map((row, index) => (
+                <React.Fragment key={index} >
+                    {row.map(cell =>
+                        <CellComponent
+                            click={click}
+                            cell={cell}
+                            key={cell.id}
+                            selected={cell.x === selectedCell?.x &&
+                                cell.y === selectedCell?.y}
+                        />
+                    )}
+                </React.Fragment>
+            ))}
+        </div >
+    )
+}
+
+export default BoardComponent
